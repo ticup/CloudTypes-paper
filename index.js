2bf0cac378aab6a0801a23cc4bf8bb2d4660b874
Tim Coppieters <coppieters.tim@gmail.com>
2016-02-25 13:40:49 +0100

    Remove Client

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
  * Created by ticup on 01/11/13.
  */
 exports.Server = require('./server/main.js');
-exports.Client = require('./client/bundle.js');
+//exports.Client = require('./client/bundle.js');
